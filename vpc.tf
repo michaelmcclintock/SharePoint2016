c878a7a1a540a273b8bad54084eff7add9ae0c8e
Michael McClintock <Mike.mcclintock@sonos.com>
2017-02-10 12:28:13 -0800

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,18 @@
+module "vpc" {
+  source = "resources/terraform/modules/vpc"
+
+  name = "test-vpc"
+
+  cidr = "10.5.0.0/16"
+  private_subnets = ["10.5.1.0/24", "10.5.2.0/24", "10.5.3.0/24"]
+  public_subnets  = ["10.5.101.0/24", "10.5.102.0/24", "10.5.103.0/24"]
+
+  enable_nat_gateway = "true"
+
+  azs      = ["us-west-2a", "us-west-2b"]
+
+  tags {
+    "Terraform" = "true"
+    "Environment" = "${var.environment}"
+  }
+}
